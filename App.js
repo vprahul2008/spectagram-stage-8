4372ac94ec8abb05e17bb497e300a753c2fff6be
vprahul2008 <73024199+vprahul2008@users.noreply.github.com>
2022-09-11 08:13:02 +0530

    Add files via upload
    spectagram stage 8

---
+++ App.js
@@ -0,0 +1,30 @@
+import *as React from 'react';
+import {createSwitchNavigator,createAppContainer} from 'react-navigation';
+
+import LoginScreen from "./screens/LoginScreen";
+import LoadingScreen from "./screens/LoadingScreen";
+import DashboardScreen from "./screens/DashBoardScreen";
+
+import *as firebase from 'firebase';
+import {firebaseConfig} from "./config"
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
+
+
+const AppSwitchNavigator = createSwitchNavigator({
+  LoadingScreen: LoadingScreen,
+  LoginScreen: LoginScreen,
+  DashboardScreen: DashboardScreen
+});
+
+const AppContainer = createAppContainer(AppSwitchNavigator)
+
+export default function App(){
+  return(
+    <AppContainer/>
+  )
+}
---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC1w-zOXgJTiYqi5W1hKx0ep0RBV17eRjM",
+    authDomain: "spectagram-42203.firebaseapp.com",
+    databaseURL: "https://spectagram-42203-default-rtdb.firebaseio.com",
+    projectId: "spectagram-42203",
+    storageBucket: "spectagram-42203.appspot.com",
+    messagingSenderId: "247871808696",
+    appId: "1:247871808696:web:b3601c89ddd85d659dc04d"
+  };
